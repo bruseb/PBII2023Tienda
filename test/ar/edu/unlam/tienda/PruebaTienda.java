b1f8663077c27746c74b8b9a688a00267f544233
Bruseb <brunosmerlo@gmail.com>
2023-11-08 17:54:53 -0300

    Primer Commit

---
+++ test/ar/edu/unlam/tienda/PruebaTienda.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.tienda;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaTienda {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
